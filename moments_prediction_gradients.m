21c019603228f3c5b8e14dcebd8801218b58b20d
Ross <ross@ross-ThinkPad-X201.(none)>
2014-09-07 16:40:36 -0400

    refactor

--- moments_prediction_gradients.m
+++ moments_prediction_gradients.m
@@ -13,7 +13,7 @@ bsz = 8; %batch size
 outsz = codesz/poolsz; %output size 
 assert(rem(outsz,1)==0,'outsz is not an integer'); 
 numerical_check = true; 
-display_sample = true; 
+display_sample = false; 
 dx = 1e-6; %step size for numerical check
 %loss multipliers 
 wl1 = 0.5; 
@@ -26,7 +26,7 @@ if ~exist('X')  %#ok<EXIST>
     X=generate_jitter_data2d(options);
     disp('done!'); 
 end
-batch = zeros(insz,3*bsz);
+batch = zeros(insz,bsz,3);
 order = im2col(randperm(size(X,2)-2),[1,bsz]); 
 batch = get_batch(X,order(:,1),batch); 
 if display_sample == true
@@ -42,7 +42,7 @@ ReLU = @(x) x.*(x>0);
 x = batch; 
 
 %codes for three temporal samples 
-z = ReLU(rand(codesz,3*bsz) - 0.5); 
+z = ReLU(rand(codesz,bsz,3) - 0.5); 
 
 %normalized decoder dictionary 
 W = rand(insz,codesz) - 0.5;
@@ -59,116 +59,127 @@ for ii = 1:outsz
 end
 P = P'*P; 
 
+%mini-batches
+Pb = P; 
+for i = 1:bsz-1
+    Pb = blkdiag(Pb,P);
+end
+P = Pb; 
+
 %check that: z = ((P'*m).*s)
-assert(norm(z-(P*z + eps*ones(size(z))).*(z./(P*z + eps*ones(size(z))))) < 1e-10); 
+% assert(norm(z-(P*z + eps*ones(size(z))).*(z./(P*z + eps*ones(size(z))))) < 1e-10); 
 
 %first moment matrix (define coordinates from -1 to +1)  
-r = zeros(1,codesz); 
-r(1:poolsz) = linspace(-1,1,poolsz); 
-M1 = zeros(outsz,codesz);
-for ii = 1:outsz
-    M1(ii,:) = r; 
-    r = circshift(r,[1 poolsz]); 
-end
-
-%L1-sparsity-error 
-El1 = @(z) sum(sum(z(:))); 
+% r = zeros(1,codesz); 
+% r(1:poolsz) = linspace(-1,1,poolsz); 
+% M1 = zeros(outsz,codesz);
+% for ii = 1:outsz
+%     M1(ii,:) = r; 
+%     r = circshift(r,[1 poolsz]); 
+% end
+% 
 
 %L2-slowness-error 
 Es = @(mag,bsz) 0.5*sum(sum((mag(:,bsz+1:2*bsz)-mag(:,1:bsz)).^2+(mag(:,2*bsz+1:3*bsz)-mag(:,bsz+1:2*bsz)).^2)); 
-
-%L2-Prediction-error 
-Ep = @(moments,bsz) 0.5*sum(sum((moments(:,2*bsz+1:3*bsz) - 2*moments(:,bsz+1:2*bsz) + moments(:,1:bsz)).^2));  
- 
-%L2-reconstruction-error 
-Er = @(x,W,z) 0.5*sum((sum((x - W*z).^2,1))); 
-
-%Loss 
-L = Er(x,W,z) + wp*Ep(M1*(z./(P*z + eps*ones(size(z)))),bsz) + ws*Es((P*z + eps*ones(size(z))),bsz) + wl1*El1(z);
-
-% Define/check gradients 
-
-%dEr/dz
-dErdz = @(x,W,z) -W'*(x - W*z); 
-
+% 
+% %L2-Prediction-error 
+% Ep = @(moments,bsz) 0.5*sum(sum((moments(:,2*bsz+1:3*bsz) - 2*moments(:,bsz+1:2*bsz) + moments(:,1:bsz)).^2));  
+%  
+% %L2-reconstruction-error 
+% Er = @(x,W,z) 0.5*sum((sum((x - W*z).^2,1))); 
+% 
+% %Loss 
+L = Er(x(:,:,1),W,z(:,:,1)); %+ wp*Ep(M1*(z./(P*z + eps*ones(size(z)))),bsz) + ws*Es((P*z + eps*ones(size(z))),bsz) + wl1*sum(abs(z(:)));
+% 
+% % Define/check gradients 
+% 
+% %dEr/dz 
+dErdz = diff_Er_dz(x,W,z); 
+% 
 %dEr/dW
-dErdW = @(x,W,z) (x - W*z)*(-z'); 
-
-%dEs/dz = (dEs/dmag)(dmag/dz)
-dEs_dz = zeros(size(z));    
-dEs_dz = diff_Es_dz(P,z,bsz,dEs_dz);
-
-% % %dEp/dz 
-% dEp_dz = diff_Ep_dz(P,M1,z,bsz); 
-% zi = reshape(numel(z(:,1:bsz);
-% Pzi = reshape((P*zi + eps*ones(size(zi))).^-2,[numel(zi),1]);
-% f = (P*diag(Pzi.^-2).*(diag(Pzi) - diag(reshape(zi,[numel(zi),1])*P); 
-
-if numerical_check == true 
-    
-    disp('checking gradients via central difference...') 
-
+dErdW = diff_Er_dW(x,W,z); 
+% 
+% %dEs/dz = (dEs/dmag)(dmag/dz)
+% dEs_dz = zeros(size(z));    
+% dEs_dz = diff_Es_dz(P,z,bsz,dEs_dz);
+% 
+% % % %dEp/dz 
+% % dEp_dz = diff_Ep_dz(P,M1,z,bsz); 
+% % zi = reshape(numel(z(:,1:bsz);
+% % Pzi = reshape((P*zi + eps*ones(size(zi))).^-2,[numel(zi),1]);
+% % f = (P*diag(Pzi.^-2).*(diag(Pzi) - diag(reshape(zi,[numel(zi),1])*P); 
+% 
+% if numerical_check == true 
+%     
+%     disp('checking gradients via central difference...') 
+% 
     %numerical check 1
     dErdz_num = zeros(size(z)); 
-    for ii = 1:size(z,1)
-        for jj = 1:size(z,2) 
-            zr = z; 
-            zl = z;
-            zr(ii,jj) = zr(ii,jj) + dx; 
-            zl(ii,jj) = zl(ii,jj) - dx; 
-            dErdz_num(ii,jj) = (Er(x,W,zr) - Er(x,W,zl))/(2*dx); 
-        end 
+    for n = 1:3 
+        for ii = 1:size(z,1)
+            for jj = 1:size(z,2) 
+                zr = z(:,:,n); 
+                zl = z(:,:,n);
+                zr(ii,jj) = zr(ii,jj) + dx; 
+                zl(ii,jj) = zl(ii,jj) - dx; 
+                dErdz_num(ii,jj,n) = (Er(x(:,:,n),W,zr) - Er(x(:,:,n),W,zl))/(2*dx); 
+            end 
+        end
     end
-    assert(norm(dErdz(x,W,z) - dErdz_num, 1)/norm(dErdz_num,1) < 1e-6); 
+    err = dErdz - dErdz_num; 
+    assert(sum(abs(err(:)))/sum(abs(dErdz(:))) < 1e-6); 
 
     %numerical check 2
     dErdW_num = zeros(size(W));  
-    for ii = 1:size(W,1) 
-        for jj = 1:size(W,2) 
-            Wr = W; 
-            Wl = W;
-            Wr(ii,jj) = Wr(ii,jj) + dx; 
-            Wl(ii,jj) = Wl(ii,jj) - dx; 
-            dErdW_num(ii,jj) = (Er(x,Wr,z) - Er(x,Wl,z))/(2*dx); 
+    for n = 1:3 
+        for ii = 1:size(W,1) 
+            for jj = 1:size(W,2) 
+                Wr = W; 
+                Wl = W;
+                Wr(ii,jj) = Wr(ii,jj) + dx; 
+                Wl(ii,jj) = Wl(ii,jj) - dx; 
+                dErdW_num(ii,jj) = dErdW_num(ii,jj) + ((Er(x(:,:,n),Wr,z(:,:,n)) - Er(x(:,:,n),Wl,z(:,:,n)))/(2*dx)); 
+            end
         end
     end
-    assert(norm(dErdW(x,W,z) - dErdW_num, 1)/norm(dErdW_num,1) < 1e-6); 
-
- 
-    %numerical check 3
-    dEsdz_num = zeros(size(z)); 
-    for ii = 1:size(z,1)
-        for jj = 1:size(z,2) 
-            zr = z; 
-            zl = z;
-            zr(ii,jj) = zr(ii,jj) + dx; 
-            zl(ii,jj) = zl(ii,jj) - dx; 
-            dEsdz_num(ii,jj) = (Es((P*zr + eps*ones(size(zr))),bsz) - Es((P*zl + eps*ones(size(zl))),bsz))/(2*dx); 
-        end 
-    end
-    assert(norm(dEs_dz - dEsdz_num, 1)/norm(dEsdz_num,1) < 1e-6); 
-% % 
+    err = dErdW - dErdW_num; 
+    assert(sum(abs(err(:)))/sum(abs(dErdW(:))) < 1e-6); 
 % 
-    % numerical check 4 
-%     dEp_dz_num = zeros(size(z)); 
+%  
+%     %numerical check 3
+%     dEsdz_num = zeros(size(z)); 
 %     for ii = 1:size(z,1)
 %         for jj = 1:size(z,2) 
 %             zr = z; 
 %             zl = z;
 %             zr(ii,jj) = zr(ii,jj) + dx; 
 %             zl(ii,jj) = zl(ii,jj) - dx; 
-%             dEp_dz_num(ii,jj) = (Ep(M1*(zr./(P*zr + eps*ones(size(zr))))) - Ep(M1*(zl./(P*zl + eps*ones(size(zl))))))/(2*dx); 
+%             dEsdz_num(ii,jj) = (Es((P*zr + eps*ones(size(zr))),bsz) - Es((P*zl + eps*ones(size(zl))),bsz))/(2*dx); 
 %         end 
 %     end
-%     
-%     unstable when group is off, so relax the accuracy (?) 
-%     assert(norm(dEp_dz - dEp_dz_num, 1)/norm(dEp_dz_num,1) < 1e-3); 
-
-    disp('passed!')
-
-end 
-
-% Training 
+%     assert(norm(dEs_dz - dEsdz_num, 1)/norm(dEsdz_num,1) < 1e-6); 
+% % % 
+% % 
+%     % numerical check 4 
+% %     dEp_dz_num = zeros(size(z)); 
+% %     for ii = 1:size(z,1)
+% %         for jj = 1:size(z,2) 
+% %             zr = z; 
+% %             zl = z;
+% %             zr(ii,jj) = zr(ii,jj) + dx; 
+% %             zl(ii,jj) = zl(ii,jj) - dx; 
+% %             dEp_dz_num(ii,jj) = (Ep(M1*(zr./(P*zr + eps*ones(size(zr))))) - Ep(M1*(zl./(P*zl + eps*ones(size(zl))))))/(2*dx); 
+% %         end 
+% %     end
+% %     
+% %     unstable when group is off, so relax the accuracy (?) 
+% %     assert(norm(dEp_dz - dEp_dz_num, 1)/norm(dEp_dz_num,1) < 1e-3); 
+% 
+%     disp('passed!')
+% 
+% end 
+% 
+% % Training 
 
 
 
