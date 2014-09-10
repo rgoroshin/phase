2a0aedc27eb5ff0a5563d250b4526f5cafaeb89e
Ross Goroshin <rgoroshin@gmail.com>
2014-09-10 16:43:24 -0400

    started training

--- train.m
+++ train.m
@@ -7,24 +7,26 @@ addpath ./fprop_bprop/
 % Constants 
 %dimensions 
 insz = 16*16; %patch size
-codesz = 16; %code size 
+codesz = 16;%2*insz; %code size 
 poolsz = 4; %pool group size
+poolst = 4; %pool group stride 
 bsz = 8; %batch size 
-L = 10000; 
+L = 100; 
 outsz = codesz/poolsz; %output size 
 assert(rem(outsz,1)==0,'outsz is not an integer'); 
 numerically_check_gradients = true; 
 display_sample = true; 
 dx = 1e-6; %step size for numerical check
 %loss multipliers 
-wL1 = 0.5; 
-ws = 0.5; 
-wp = 0.5; 
+wL1 = 0.0; 
+ws = 0.0; 
+wp = 0.0; 
 % Generate toy dataset 
 if ~exist('X')  %#ok<EXIST>
     disp('generating dataset...'); 
     options = struct('ntemplates',2,'L',L); 
     X=generate_jitter_data2d(options);
+    X = X - repmat(mean(X,2),[1 size(X,2)]); 
     disp('done!'); 
 end
 batch = zeros(insz,bsz,3);
@@ -56,7 +58,7 @@ r(1,1:poolsz) = 1;
 P1 = zeros(outsz,codesz); 
 for ii = 1:outsz
     P1(ii,:) = r; 
-    r = circshift(r,[1 poolsz]); 
+    r = circshift(r,[1 poolst]); 
 end
 P = P1'*P1; 
 
@@ -66,7 +68,7 @@ r(1:poolsz) = linspace(-1,1,poolsz);
 M1 = zeros(outsz,codesz);
 for ii = 1:outsz
     M1(ii,:) = r; 
-    r = circshift(r,[1 poolsz]); 
+    r = circshift(r,[1 poolst]); 
 end
 
 %mini-batches
@@ -100,48 +102,59 @@ if numerically_check_gradients == true
 end 
 
 % Training 
-epochs = 1; 
+epochs = 100; 
 infer_steps = 20; 
+zstep = 0.01; 
+Wstep = 0.05; 
 
 for iter = 1:epochs 
-    
-    waitbar(iter/epochs); 
     
     Loss = 0; 
     
     for n = 1:size(order,2)
+        
+         waitbar(n/size(order,2)); 
        
-         x = get_batch(X,order(:,n),batch); 
+         x = get_batch(X,order(:,n),x); 
          
-         RecCost = Er(x,W,z); 
-         L1Cost = sum(abs(z(:))); 
-         SlowCost = Es(z,P1);
-         [PredCost,moments_error] = Ep(z,P,M1);  
+         RecCost = 2*Er(x,W,z)/(norm(x(:))^2); 
+%          L1Cost = sum(abs(z(:))); 
+%          SlowCost = Es(z,P1);
+%          PredCost = Ep(z,P,M1);  
          
-         Lb = RecCost + wL1*L1Cost + ws*SlowCost + wp*PredCost; 
-         Loss = Loss + Lb; 
+         Loss_batch = RecCost; %+ wL1*L1Cost + ws*SlowCost + wp*PredCost; 
+         Loss = Loss + Loss_batch; 
          
-         %inference 
+         %inference (z-update) 
          z = zeros(codesz,bsz,3); 
          
          for ii = 1:infer_steps 
             
-             dEr_dz = diff_Er_dz(x,W,z);
-             dEs_dz = diff_Es_dz(z,P);
-             dEp_dz = diff_Ep_dz(z,moments_error,P,M1);
-             
+%              dEr_dz = diff_Er_dz(x,W,z);
+%              dEs_dz = diff_Es_dz(z,P);
+%              dEp_dz = diff_Ep_dz(z,P,M1);            
+             dz = diff_Er_dz(x,W,z);%+ ws*dEs_dz + wp*dEp_dz;  
+%              z = ReLU(z - zstep*dz, zstep*wL1); 
+             z = z - zstep*dz; 
              
          end
          
+         %dictionary update 
+         dW = diff_Er_dW(x,W,z);
+         W = W - Wstep*dW; 
+         W = normalize_cols(W,insz); 
         
     end
     
-    disp(['Loss = ' num2str(Loss)])
+    Iw = reshape(W,[sqrt(insz),sqrt(insz),1,codesz]); 
+    figure(1); 
+    imdisp(Iw,'Border',[0.1 0.1])
+    disp(['Loss = ' num2str(Loss/L)])
     
 end
 
 
-
+% imdisp(Ibatch(:,:,:,2,:),'Border',[0.1 0.1]); 
 
 
 
